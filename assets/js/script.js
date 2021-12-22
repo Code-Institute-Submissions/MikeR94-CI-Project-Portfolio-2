979cfb1b81e9a34886bf6c16005d57f24dec0a23
miker94 <michael_ralph@hotmail.com>
2021-12-22 16:09:36 +0000

    Add basic functionality to allow user to select category and then take the user to the question screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,28 @@ let nextBtn = document.getElementById("next-btn").addEventListener("click", toCa
 let homeBtn1 = document.getElementById("home-icon-1").addEventListener("click", homeButton);
 let homeBtn2 = document.getElementById("home-icon-2").addEventListener("click", homeButton);
 let homeBtn3 = document.getElementById("home-icon-3").addEventListener("click", homeButton);
-
+let generalKnowledgeBtn = document.getElementById("general-knowledge-btn").addEventListener("click", runGeneralKnowledgeQuiz);
+let constructorsBtn = document.getElementById("constructors-btn").addEventListener("click", runConstructorQuiz);
+let driversBtn = document.getElementById("drivers-btn").addEventListener("click", runDriverQuiz);
 
 function toCategoryGameArea() {
   document.getElementById("start-game-area").classList.add("hide");
   document.getElementById("category-game-area").classList.remove("hide");
+}
+
+function runDriverQuiz() {
+  document.getElementById("category-game-area").classList.add("hide");
+  document.getElementById("question-game-area").classList.remove("hide");
+}
+
+function runConstructorQuiz() {
+  document.getElementById("category-game-area").classList.add("hide");
+  document.getElementById("question-game-area").classList.remove("hide");
+}
+
+function runGeneralKnowledgeQuiz() {
+  document.getElementById("category-game-area").classList.add("hide");
+  document.getElementById("question-game-area").classList.remove("hide");
 }
 
 function homeButton() {
