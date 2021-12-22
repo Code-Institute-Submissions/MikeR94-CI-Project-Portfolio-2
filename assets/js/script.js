4256243c4617492a0d945a3743c40bb0fcd7776f
miker94 <michael_ralph@hotmail.com>
2021-12-22 15:04:30 +0000

    Add home icon functionality to return the user to the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 let nextBtn = document.getElementById("next-btn").addEventListener("click", toCategoryGameArea);
+let homeBtn1 = document.getElementById("home-icon-1").addEventListener("click", homeButton);
+let homeBtn2 = document.getElementById("home-icon-2").addEventListener("click", homeButton);
+let homeBtn3 = document.getElementById("home-icon-3").addEventListener("click", homeButton);
+
 
 function toCategoryGameArea() {
-    document.getElementById("start-game-area").classList.add("hide");
-    document.getElementById("category-game-area").classList.remove("hide");
+  document.getElementById("start-game-area").classList.add("hide");
+  document.getElementById("category-game-area").classList.remove("hide");
+}
+
+function homeButton() {
+  window.location.reload();
 }
