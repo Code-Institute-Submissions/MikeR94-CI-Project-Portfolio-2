0dd24ef7921a44ae2102d419ebf816aee7aa6c69
miker94 <michael_ralph@hotmail.com>
2021-12-22 09:29:22 +0000

    Add javascript functionality to allow user to move to category screen

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let nextBtn = document.getElementById("next-btn").addEventListener("click", toCategoryGameArea);
+
+function toCategoryGameArea() {
+    document.getElementById("start-game-area").classList.add("hide");
+    document.getElementById("category-game-area").classList.remove("hide");
+    console.log("I have been clicked");
+}
