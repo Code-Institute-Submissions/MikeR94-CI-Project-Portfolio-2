a3adab8d5bad1410acec2f8842b1fc87ebfcfdfc
miker94 <michael_ralph@hotmail.com>
2021-12-24 11:10:24 +0000

    Add 20 questions for each category in a new questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,482 @@
+let driverQuestions = [{
+    question: 'F1 driver question 1',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 2',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 3',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 4',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 5',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 6',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 7',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 8',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 9',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 10',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 11',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 12',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 13',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 14',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 15',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 16',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 17',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 19',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 19',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 driver question 20',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },];
+  
+  let constructorQuestions = [{
+    question: 'F1 constructor question 1',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 2',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 3',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 4',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 5',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 6',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 7',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 8',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 9',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 10',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 11',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 12',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 13',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 14',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 15',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 16',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 17',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 18',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 19',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 constructor question 20',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  }];
+  
+  let generalKnowledgeQuestions = [{
+    question: 'F1 general knowledge question 1',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 2',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 3',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 4',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 5',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 6',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 7',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 8',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 9',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 10',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 11',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 12',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 13',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 14',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 15',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 16',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 17',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 18',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 19',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  },
+  {
+    question: 'F1 general knowledge question 20',
+    a: 'F1 Answer 1',
+    b: 'F1 Answer 2',
+    c: 'F1 Answer 3',
+    d: 'F1 Answer 4',
+    answer: 'A'
+  }];
