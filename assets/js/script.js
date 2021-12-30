2af51bfcc5ecc423078d293d0d70590d55d005ec
miker94 <michael_ralph@hotmail.com>
2021-12-30 18:37:38 +0000

    Style hiscores section to add trophys. Remove text on hiscores page and also the button clearing the hiscores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 let nextBtn = document.getElementById("next-btn").addEventListener("click", toDifficultyGameArea);
 let loadNextQuestion = document.getElementById("next-question").addEventListener("click", nextQuestion);
-let clearHiscoresBtn = document.getElementById("reset-hiscores").addEventListener("click", clearHiscores);
 let answer1 = document.getElementById("answer1-btn");
 let answer2 = document.getElementById("answer2-btn");
 let answer3 = document.getElementById("answer3-btn");
@@ -309,7 +308,7 @@ function updateHiscore() {
 }
 
 /**
- * Clears the hiscores and reloads the website
+ * Clears the hiscores and reloads the website (currently not is use)
  */
 function clearHiscores() {
   localStorage.clear();
