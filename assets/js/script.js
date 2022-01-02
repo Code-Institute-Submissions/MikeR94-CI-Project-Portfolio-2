ef19c49e1fa5694859436a186ecd2bb80fdb632a
miker94 <michael_ralph@hotmail.com>
2022-01-02 15:30:03 +0000

    Remove questionsAnswered variable causing quiz to break

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,7 +125,6 @@ function checkAnswer() {
     stopTimer();
     correctSound();
     showNextQuestionIcon();
-    questionsAnsweredCorrectly++;
   } else {
     answerClicked = true;
     determineColour = "incorrect";
