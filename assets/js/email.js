b9c47bdc289a801a259ac5b266d6fcde8f337bac
miker94 <michael_ralph@hotmail.com>
2022-01-04 16:56:40 +0000

    Add functionality to disable the submit button when the player clicks it (sent and error) and reset styles back to normal

--- assets/js/email.js
+++ assets/js/email.js
@@ -11,12 +11,37 @@ function sendMail() {
     message: userMessage.value,
   };
 
-  emailjs.send("service_al2h1zo", "template_xqi4jaf", emailProperties).then(function (respond) {
-    console.log("Success", respond);
-    document.getElementById("contact-submit").innerHTML = "Sent";
-    document.getElementById("contact-submit").classList.add("correct");
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
-  });
+  if (userName.value === "" || email.value === "" || message.value === "") {
+        document.getElementById("contact-submit").innerHTML = "Error";
+        document.getElementById("contact-submit").classList.add("wrong");
+        document.getElementById("contact-submit").classList.remove("hover");
+        document.getElementById("contact-submit").disabled = true;
+        setTimeout(resetError, 3000);
+  } else {
+    emailjs.send("service_al2h1zo", "template_xqi4jaf", emailProperties).then(function (respond) {
+        console.log("Success", respond);
+        document.getElementById("contact-submit").innerHTML = "Sent";
+        document.getElementById("contact-submit").classList.add("correct");
+        document.getElementById("contact-submit").classList.remove("hover");
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("contact-submit").disabled = true;
+        setTimeout(resetSent, 3000);
+      });
+  }
+}
+
+function resetError() {
+    document.getElementById("contact-submit").innerHTML = "Submit";
+    document.getElementById("contact-submit").classList.remove("wrong");
+    document.getElementById("contact-submit").classList.add("hover");
+    document.getElementById("contact-submit").disabled = false;
+}
+
+function resetSent() {
+    document.getElementById("contact-submit").innerHTML = "Submit";
+    document.getElementById("contact-submit").classList.remove("correct");
+    document.getElementById("contact-submit").classList.add("hover");
+    document.getElementById("contact-submit").disabled = false;
 }
