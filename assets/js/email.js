29b8bd85a703b20016db79b7aafac20a7138ae99
miker94 <michael_ralph@hotmail.com>
2022-01-11 16:31:40 +0000

    Add functionality to check the users inputs on the contact form so that they meet the form validation criteria. Display to the user which field is currently incorrect

--- assets/js/email.js
+++ assets/js/email.js
@@ -4,6 +4,9 @@ let userMessage = document.getElementById("message");
 
 let submit = document.getElementById("contact-submit").addEventListener("click", sendMail);
 
+let regExEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let regExName = /^(?! )[A-Za-z\s]*$/;
+
 function sendMail() {
   let emailProperties = {
     name: userName.value,
@@ -11,37 +14,66 @@ function sendMail() {
     message: userMessage.value,
   };
 
-  if (userName.value === "" || email.value === "" || message.value === "") {
-        document.getElementById("contact-submit").innerHTML = "Error";
-        document.getElementById("contact-submit").classList.add("wrong");
-        document.getElementById("contact-submit").classList.remove("hover");
-        document.getElementById("contact-submit").disabled = true;
-        setTimeout(resetError, 3000);
-  } else {
+  if (userName.value.match(regExName) && userName.value != null && userName.value != undefined && userName.value != "" && email.value.match(regExEmail) && message.value.length > 10) {
     emailjs.send("service_al2h1zo", "template_xqi4jaf", emailProperties).then(function (respond) {
-        console.log("Success", respond);
-        document.getElementById("contact-submit").innerHTML = "Sent";
-        document.getElementById("contact-submit").classList.add("correct");
-        document.getElementById("contact-submit").classList.remove("hover");
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        document.getElementById("contact-submit").disabled = true;
-        setTimeout(resetSent, 3000);
-      });
+      console.log("Success", respond);
+      document.getElementById("contact-submit").innerHTML = "Sent";
+      document.getElementById("contact-submit").classList.add("correct");
+      document.getElementById("contact-submit").classList.remove("hover");
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("contact-submit").disabled = true;
+      setTimeout(resetSent, 3000);
+    });
   }
-}
+  if (userName.value == null || userName.value == undefined || userName.value == "") {
+    errorButton();
+    setTimeout(resetError, 3000);
+    console.log("username incorrect");
+    document.getElementById("name").classList.add("error-border");
+  }
+  if (!userName.value.match(regExName)) {
+    errorButton();
+    setTimeout(resetError, 3000);
+    console.log("username incorrect");
+    document.getElementById("name").classList.add("error-border");
+  } 
+  if (!email.value.match(regExEmail)) {
+    errorButton();
+    setTimeout(resetError, 3000);
+    console.log("email incorrect");
+    document.getElementById("email").classList.add("error-border");
+  } 
+  if (message.value.length < 10) {
+    errorButton();
+    setTimeout(resetError, 3000);
+    console.log("message not long enough");
+    document.getElementById("message").classList.add("error-border");
+  } 
+} 
 
 function resetError() {
-    document.getElementById("contact-submit").innerHTML = "Submit";
-    document.getElementById("contact-submit").classList.remove("wrong");
-    document.getElementById("contact-submit").classList.add("hover");
-    document.getElementById("contact-submit").disabled = false;
+  document.getElementById("contact-submit").innerHTML = "Submit";
+  document.getElementById("contact-submit").classList.remove("wrong");
+  document.getElementById("contact-submit").classList.add("hover");
+  document.getElementById("contact-submit").disabled = false;
+  document.getElementById("name").classList.remove("error-border");
+  document.getElementById("email").classList.remove("error-border");
+  document.getElementById("message").classList.remove("error-border");
 }
 
 function resetSent() {
-    document.getElementById("contact-submit").innerHTML = "Submit";
-    document.getElementById("contact-submit").classList.remove("correct");
-    document.getElementById("contact-submit").classList.add("hover");
-    document.getElementById("contact-submit").disabled = false;
+  document.getElementById("contact-submit").innerHTML = "Submit";
+  document.getElementById("contact-submit").classList.remove("correct");
+  document.getElementById("contact-submit").classList.add("hover");
+  document.getElementById("contact-submit").disabled = false;
 }
+
+function errorButton() {
+  document.getElementById("contact-submit").innerHTML = "Error";
+  document.getElementById("contact-submit").classList.add("wrong");
+  document.getElementById("contact-submit").classList.remove("hover");
+  document.getElementById("contact-submit").disabled = true;
+}
+
