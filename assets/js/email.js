f3fa75541ef7b056045d4caa2ce304af1d4251bd
miker94 <michael_ralph@hotmail.com>
2022-01-12 11:00:25 +0000

    Add a message sent message for the user to let them know that their message has been successfully sent

--- assets/js/email.js
+++ assets/js/email.js
@@ -17,14 +17,13 @@ function sendMail() {
   if (userName.value.match(regExName) && userName.value != null && userName.value != undefined && userName.value != "" && email.value.match(regExEmail) && message.value.length > 10) {
     emailjs.send("service_al2h1zo", "template_xqi4jaf", emailProperties).then(function (respond) {
       console.log("Success", respond);
-      document.getElementById("contact-submit").innerHTML = "Sent";
-      document.getElementById("contact-submit").classList.add("correct");
-      document.getElementById("contact-submit").classList.remove("hover");
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("message").value = "";
-      document.getElementById("contact-submit").disabled = true;
-      setTimeout(resetSent, 3000);
+      document.getElementById("contact-form").classList.add("hide");
+      document.getElementById("contact-name-question").classList.add("hide");
+      document.getElementById("contact-email-question").classList.add("hide");
+      document.getElementById("contact-message-question").classList.add("hide");
+      document.getElementById("contact-title-text").classList.add("hide");
+      document.getElementById("contact-go-home").classList.remove("hide");
+      document.getElementById("sent-message").classList.remove("hide");
     });
   }
   if (userName.value == null || userName.value == undefined || userName.value == "") {
@@ -61,13 +60,6 @@ function resetError() {
   document.getElementById("name").classList.remove("error-border");
   document.getElementById("email").classList.remove("error-border");
   document.getElementById("message").classList.remove("error-border");
-}
-
-function resetSent() {
-  document.getElementById("contact-submit").innerHTML = "Submit";
-  document.getElementById("contact-submit").classList.remove("correct");
-  document.getElementById("contact-submit").classList.add("hover");
-  document.getElementById("contact-submit").disabled = false;
 }
 
 function errorButton() {
